646aadbf08dfa82f43263c39165a4a5ea96a6b57
Himanshu sekhar sahoo <himanshusekhar836@gmail.com>
2023-02-23 11:45:11 +0530

    routes set up

--- app.js
+++ app.js
@@ -4,6 +4,37 @@ const app= express();
 
 const PORT = process.env.PORT || 3210;
 
+app.use(express.json())
+
+app.get('/user/:id',(req,resp)=>{
+    resp.status(200).send({
+        status:"user with ID"+req.params.id
+    })
+})
+
+app.post('/user',(req,resp)=>{
+    resp.status(200).send({
+        status:"user created",
+        data:req.body
+    })
+})
+
+app.put('/user/:id',(req,resp)=>{
+    resp.status(200).send({
+        status:"user updated with id"+req.params.id
+    })
+})
+
+
+app.delete('/user/:id',(req,resp)=>{
+    resp.status(200).send({
+        status:"user deleted with id"+req.params.id
+    })
+})
+
+
+
+
 app.listen(PORT,()=>{
-    console.log('server running');
+    console.log('server running on port 3210');
 });
